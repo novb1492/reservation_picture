e650ea1d81d26b6742f40f49dbad4a8354e060f2
김준영 <novb1492@naver.com>
2023-01-17 20:13:39 +0900

    버그fix

--- src/pages/pop/ReservationProPage.js
+++ src/pages/pop/ReservationProPage.js
@@ -106,7 +106,7 @@ function ReservationProPage() {
         slidesPerView={getNum()}>
         {state.ReserReducers.products.map(product => {
           return (
-            <SwiperSlide key={`${product.id}div`}  onClick={() => { choice(product) }}>
+            <SwiperSlide key={`${Math.random()}`}  onClick={() => { choice(product) }}>
               <img key={`${product.id}img`} className="product_img" src={product.img} />
               <p key={`${product.id}n`}>{product.name}</p>
               <p key={`${product.id}p`}>{product.price}원</p>
