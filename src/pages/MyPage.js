9f20e2882594aa2bfacd3c5acc12b3bba45bdffc
김준영 <novb1492@naver.com>
2023-01-19 16:28:59 +0900

    취소 불가 내용 추가

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -101,6 +101,8 @@ function MyPage() {
         <p>결제금액: {priceComma(state.ReserReducers.reservationInfo.totalPrice)}원</p>
         <p>이용시간:{state.ReserReducers.reservationInfo.totalTime}시간</p>
         <p>최소결제금액:{priceComma(state.ReserReducers.reservationInfo.minPrice)}원</p>
+        <p>상품 주문 접수완료시 취소 불가</p>
+        <p>가장 빠른 예약 시간 30분전 부터 취소불가</p>
         {state.ReserReducers.reservationInfo.refund === true ? <button>전체 취소</button> : <p>취소 불가</p>}
       </div>
       <hr></hr>
